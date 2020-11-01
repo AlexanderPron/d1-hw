cd6ba5cfeb0150385caa448b4d76509150f8e4b3
Gadya <zakuson2001@inbox.ru>
2020-11-02 01:46:46 +0300

    HW done

---
+++ config.py
@@ -0,0 +1,6 @@
+auth_params = {    
+  'key': "xxx",    
+  'token': "xxx", 
+  }  
+base_url = 'https://api.trello.com/1/{}'
+board_id = 'xxx'
--- trello_client.py
+++ trello_client.py
@@ -1,13 +1,6 @@
 import requests
 import sys
-
-auth_params = {    
-  'key': "de929ad698e8c565a65d6e0c37c5c2e4",    
-  'token': "096d4b4c6f1b0925b29a7ac3e5e90d0cd8558efba8d93f2c425f726694d4d20b", 
-  }  
-   
-base_url = "https://api.trello.com/1/{}"
-board_id = "5B6gGDGZ"  
+from config import auth_params, base_url, board_id
 
 def read():
   column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()       
@@ -28,22 +21,6 @@ def createTask(name, column_name):
       return True
   print('Листа с названием {} не существует!'.format(column_name))
   return False
-
-def move(name, column_name):     
-  column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
-  task_id = None    
-  for column in column_data:    
-    column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()    
-    for task in column_tasks:    
-      if task['name'] == name:    
-        task_id = task['id']    
-        break    
-      if task_id:    
-        break    
-  for column in column_data:    
-    if column['name'].split('(')[0].rstrip() == column_name:        
-      requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})    
-      break  
 
 def getLongBoardId(board_id):
   board_data = requests.get(base_url.format('boards') + '/' + board_id, params=auth_params).json()
@@ -78,13 +55,19 @@ def updateColumnName():
 
 def getAvailableLists():
   column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-  listsDict = {}
-  i = 0
+  allLists = []
   for column in column_data:
-    i += 1
-    listsDict[str(i)] = column['name'].split('(')[0].rstrip()
-  return listsDict
+    allLists.append(column)
+  return allLists
 
+def getAvailableTasks():
+  allTasks = []
+  column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()       
+  for column in column_data:               
+    task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+    for task in task_data:
+      allTasks.append(task)
+  return allTasks
 
 if __name__ == "__main__":
   try:
@@ -106,42 +89,104 @@ if __name__ == "__main__":
     if (choose == '1'):
       updateColumnName()
       read()
+
     elif (choose == '2'):
       new_column_name = input('Введите название нового листа: -> ')
       columnCreate(new_column_name)
       updateColumnName()
+
     elif (choose == '3'):
-      availableLists = {}
       availableLists = getAvailableLists()
+      i = 0
       print('\nДоступные листы:')
       for availableList in availableLists:
-        print('\t{} - {}'.format(availableList, availableLists[availableList]))
-      column_name = input('Введите номер листа, который необходимо отправить в архив: -> ')
+        i += 1
+        print('\t{} - {}'.format(i, availableList['name'].split('(')[0].rstrip()))
       try:
-        archiveList(availableLists[column_name])
-      except KeyError:
+        column_name_archive = int(input('Введите номер листа, который необходимо отправить в архив: -> '))
+        if (column_name_archive < 1):
+          print('Номер листа должен быть больше 0')
+          continue
+        else:
+          archiveList(availableLists[column_name_archive - 1]['name'].split('(')[0].rstrip())
+      except ValueError:
+        print('\nНе корректный номер листа. Необходимо ввести число')
+        continue
+      except LookupError:
         print('\nНе корректный номер листа')
+        continue
+
     elif (choose == '4'):
-      availableLists = {}
+      availableLists = []
       availableLists = getAvailableLists()
+      i = 0
       print('\nДоступные листы:')
       for availableList in availableLists:
-        print('\t{} - {}'.format(availableList, availableLists[availableList]))
-      column_name = input('Введите номер листа, в который необходимо добавить задачу: -> ')
+        i += 1
+        print('\t{} - {}'.format(i, availableList['name'].split('(')[0].rstrip()))
       try:
-        availableLists[column_name]
-      except KeyError:
+        column_name_add = int(input('Введите номер листа, в который необходимо добавить задачу: -> '))
+        if (column_name_add < 1):
+          print('Номер листа должен быть больше 0')
+          continue
+        else:
+          availableLists[column_name_add - 1]
+      except ValueError:
+        print('\nНе корректный номер листа. Необходимо ввести число')
+        continue
+      except LookupError:
         print('\nНе корректный номер листа')
         continue
       name = input('Введите текст новой задачи: -> ')
-      createTask(name, availableLists[column_name])
+      createTask(name, availableLists[column_name_add - 1]['name'].split('(')[0].rstrip())
       updateColumnName()
+
     elif (choose == '5'):
-      print('Перемещаем задачу..')
+      i = 0
+      print('Доступные задачи для перемещения:\n')
+      allTasks = getAvailableTasks()
+      dictLists = {}
+      allLists = getAvailableLists()
+      for availableList in allLists:
+        dictLists[availableList['id']] = availableList['name'] 
+      for task in allTasks:
+        i += 1
+        print('\t{} - {} (id - {} в колонке "{}")'.format(i,task['name'],task['id'], dictLists[task['idList']].split('(')[0].rstrip()))
+      try:
+        task_number = int(input('Введите номер задачи, которую необходимо переместить: -> '))
+        if (task_number < 1):
+          print('Номер задачи должен быть больше 0')
+          continue
+        else:
+          allTasks[task_number - 1]
+      except ValueError:
+        print('\nНе корректный номер задачи. Необходимо ввести число')
+        continue
+      except LookupError:
+        print('\nНе корректный номер задачи')
+        continue
+      task_id = allTasks[task_number - 1]['id']
+      print('\nДоступные листы:')
+      i = 0
+      for availableList in allLists:
+        i += 1
+        print('\t{} - {}'.format(i, availableList['name'].split('(')[0].rstrip()))
+      try:
+        column_number = int(input('Введите номер листа, в который необходимо переместить задачу: -> '))
+        if (column_number < 1):
+          print('Номер листа должен быть больше 0')
+          continue
+        else:
+          allLists[column_number - 1]
+      except ValueError:
+        print('\nНе корректный номер листа. Необходимо ввести число')
+        continue
+      except LookupError:
+        print('\nНе корректный номер листа')
+        continue
+      requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': allLists[column_number - 1]['id'], **auth_params})
+      updateColumnName()
       
     elif (choose == '6'):
       print('Завершение программы..')
       sys.exit()
-
-
-    
